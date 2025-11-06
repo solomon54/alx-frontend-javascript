7551706f5773544ce53dd05beb0bde88806cd8a1
solomon54 <solomontsehay54@gmail.com>
2025-11-06 20:24:40 +0300

    working with interface

--- 0x04-TypeScript/task_1/js/main.js
+++ 0x04-TypeScript/task_1/js/main.js
@@ -14,10 +14,24 @@ const director1 = {
     numberOfReports: 5,
 };
 console.log(director1);
-const printTeacher = (firstName, lastName) => {
+// interface printTeacherFunction {
+//   (firstName: string, lastName: string): string;
+// }
+// function printTeacher({
+//   firstName,
+//   lastName,
+// }: {
+//   firstName: string;
+//   lastName: string;
+// }): string {
+//   firstName = firstName.charAt(0);
+//   return `${firstName}. ${lastName}`;
+// }
+// console.log(printTeacher("Solomon", "Tsehay"));
+function printTeacher({ firstName, lastName, }) {
     firstName = firstName.charAt(0);
     return `${firstName}. ${lastName}`;
-};
-console.log(printTeacher("Solomon", "Tsehay"));
+}
+console.log(printTeacher({ firstName: "Solomon", lastName: "Tsehay" }));
 export {};
 //# sourceMappingURL=main.js.map
--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -30,12 +30,31 @@ const director1: Director = {
 
 console.log(director1);
 
-interface printTeacherFunction {
-  (firstName: string, lastName: string): string;
-}
-const printTeacher: printTeacherFunction = (firstName, lastName) => {
+// interface printTeacherFunction {
+//   (firstName: string, lastName: string): string;
+// }
+// function printTeacher({
+//   firstName,
+//   lastName,
+// }: {
+//   firstName: string;
+//   lastName: string;
+// }): string {
+//   firstName = firstName.charAt(0);
+//   return `${firstName}. ${lastName}`;
+// }
+
+// console.log(printTeacher("Solomon", "Tsehay"));
+
+function printTeacher({
+  firstName,
+  lastName,
+}: {
+  firstName: string;
+  lastName: string;
+}): string {
   firstName = firstName.charAt(0);
   return `${firstName}. ${lastName}`;
-};
+}
 
-console.log(printTeacher("Solomon", "Tsehay"));
+console.log(printTeacher({ firstName: "Solomon", lastName: "Tsehay" }));
